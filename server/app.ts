56c0a897a5daa8d9122158a6fa5b81bbd87e0c04
Saad Shamsi <114074040+saad-shamsi@users.noreply.github.com>
2023-09-22 17:13:58 +0500

    db connected

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,35 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+require("dotenv").config();
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors=> cross origin resource sharing
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// testing API
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+// unknown route handler
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`) as any;
+  err.statusCode = 404;
+  err.status = "fail";
+  next(err);
+});
